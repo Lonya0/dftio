6321cc58e32eb051ae45f073dfa1a07c3edc8ba8
qqgu <guqq_phy@qq.com>
2025-03-31 20:02:20 +0800

    feat: add VASPParser for VASP file handling and update __init__.py

--- dftio/io/__init__.py
+++ dftio/io/__init__.py
@@ -2,11 +2,13 @@ from .abacus.abacus_parser import AbacusParser
 from .rescu.rescu_parser import RescuParser
 from .gaussian.gaussian_parser import GaussianParser
 from .siesta.siesta_parser import SiestaParser
+from .vasp.vasp_parser import VASPParser
 
 
 __all__ = [
     "AbacusParser",
     "RescuParser",
     "GaussianParser",
-    "SiestaParser"
+    "SiestaParser",
+    "VASPParser"
 ]
---
+++ dftio/io/vasp/vasp_parser.py
@@ -0,0 +1,76 @@
+from scipy.linalg import block_diag
+import re
+from tqdm import tqdm
+from collections import Counter
+from ...constants import orbitalId
+import ase
+from ase.io import read, write
+import dpdata
+import os
+import numpy as np
+from ..parse import Parser, ParserRegister, find_target_line
+from ...data import _keys
+from ...register import Register
+
+@ParserRegister.register("vasp")
+class VASPParser(Parser):
+    def __init__(
+            self,
+            root,
+            prefix,
+            **kwargs
+            ):
+        super(VASPParser, self).__init__(root, prefix)
+        self.raw_sys = [read(self.raw_datas[idx]+'/POSCAR') for idx in range(len(self.raw_datas))]
+    
+    # essential
+    def get_structure(self, idx):
+        sys = self.raw_sys[idx]
+        
+        structure = self.ase_to_structure(sys)
+
+        return structure
+    
+    
+    # essential
+    def get_eigenvalue(self, idx):
+        path = self.raw_datas[idx]
+        assert os.path.exists(os.path.join(path, "EIGENVAL"))
+        kpts, eigs = self.read_EIGENVAL(os.path.join(path, "EIGENVAL"))
+
+        return {_keys.ENERGY_EIGENVALUE_KEY: eigs, _keys.KPOINT_KEY: kpts}
+    
+    @staticmethod
+    def read_EIGENVAL(file):
+        Nhse = 0 # number of HSE bands, used for HSE, 
+        f = open(file, 'r')
+        data = f.readlines()
+        # Read the number of bands
+        NBND = int(re.findall('[0-9]+', data[5])[2])
+        k_list = []
+        k_bands = []
+        kb_temp = []
+        kb_count = 0
+        for i in range(7+(NBND+2)*Nhse, len(data)):
+            temp = re.findall('[0-9\-\.\+E]+', data[i])
+            if not temp:
+                continue
+            if len(temp) == 4:
+                kt = (np.array([float(i) for i in temp[0:3]])).tolist()
+                k_list.append(kt)
+            else:
+                kb_temp.append(float(temp[1])) # the energy.
+                kb_count += 1
+                if kb_count == NBND:
+                    k_bands.append(sorted(kb_temp))
+                    kb_temp = []
+                    kb_count = 0
+        k_bands = np.array(k_bands)[np.newaxis, :, :] # [1, nk, nbands]
+        k_list = np.asarray(k_list) # [nk, 3] 
+
+        return k_list, k_bands
+
+ 
+    def get_blocks(self, idx, hamiltonian: bool=False, overlap: bool=False, density_matrix: bool=False):
+        raise NotImplementedError("VASP does not support block parsing yet.")
+
