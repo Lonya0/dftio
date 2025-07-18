585dbf4ea3ba9b3b67154cc6dae04590b0c9b7e3
Franklalalala <42742342+Franklalalala@users.noreply.github.com>
2025-07-18 16:55:31 +0800

    fix: optimize the example for separated dump folders

--- test/test_abacus_add_h0.py
+++ test/test_abacus_add_h0.py
@@ -16,15 +16,16 @@ def main():
         import shutil
         shutil.rmtree(outroot)
 
-    old_lmdb_path = os.path.join(outroot, "data.{}.lmdb".format(os.getpid()))
-    new_lmdb_path = os.path.join(outroot, "data.h0.lmdb")
+    raw_outroot = os.path.join(outroot, 'raw')
+    old_lmdb_path = os.path.join(raw_outroot, "data.{}.lmdb".format(os.getpid()))
+    new_lmdb_path = os.path.join(outroot, 'h0', "data.h0.lmdb")
     for idx in tqdm(range(num_entries)):
         parser.write(
             idx=int(idx),
             format='lmdb',
             hamiltonian=True,
             overlap=True,
-            outroot=outroot,
+            outroot=raw_outroot,
             eigenvalue=True,
             density_matrix=True,
             band_index_min=0
@@ -33,7 +34,7 @@ def main():
         h0_src_root= r'/share/mp_20_abacus_production/15_elements_0524/50_H0_workspace_v2/cooking',
         old_lmdb_path=old_lmdb_path,
         new_lmdb_path=new_lmdb_path,
-        keep_old_lmdb=False
+        keep_old_lmdb=True
     )
 
 
